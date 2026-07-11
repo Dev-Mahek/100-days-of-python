d0616fd0ee58f982529546393cf2387c5aaca48d
Mahek Roy <dev-mahek@zohomail.in>
2026-07-11 12:14:32 +0530

    Update Practice.py

--- Day-001/Practice.py
+++ Day-001/Practice.py
@@ -1,1 +1,22 @@
-print("hello world")
+name = "Arindom"
+age = 24
+height = 5.9
+is_student = True
+
+print(name)
+print(age)
+
+print("My name is", name)
+
+name = input("Enter your name: ")
+
+print(name)
+
+city = input("Where do you live? ")
+
+print("You live in", city)
+
+age = int(input("Enter age: "))
+
+print(age)
+print(type(age))
