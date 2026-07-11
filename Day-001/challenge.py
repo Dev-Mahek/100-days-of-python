79d0f69b2d206b0714d6b1c8cae6a595f481fa90
Mahek Roy <dev-mahek@zohomail.in>
2026-07-11 12:33:19 +0530

    Update challenge.py

--- Day-001/challenge.py
+++ Day-001/challenge.py
@@ -1,1 +1,43 @@
-# Challange
+"""
+Create variables for
+
+Your name
+Your age
+Your city
+Your favorite food
+
+Print them.
+"""
+
+Name: Arindom
+Age: 24
+City: Silchar
+Favorite Food: Biryani
+
+"""
+A Simple Calculator
+"""
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+sum = num1 + num2
+
+print("Sum =", sum)
+
+"""
+Ask the user for their
+
+Name
+Age
+
+Print
+Hello Arindom!
+You are 24 years old.
+"""
+name = input("Enter name ")
+age = input("Enter age ")
+
+greet = f"Hello, {name}!\nYou are {age} years old."
+
+print(greet)
