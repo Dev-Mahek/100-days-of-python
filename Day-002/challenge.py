76e71b5854fa696abeb918ec495a7571a05fc094
Mahek Roy <dev-mahek@zohomail.in>
2026-07-12 07:46:51 +0530

    Update challenge.py

--- Day-002/challenge.py
+++ Day-002/challenge.py
@@ -12,3 +12,15 @@ print("Months:", months)
 print("Monthly EMI: ₹", round(emi, 2))
 
 """
+Tip Calculator
+"""
+
+bill = float(input("Enter Bill Amount: ₹"))
+tip_percentage = float(input("Enter Tip Percentage (%): "))
+
+tip_amount = bill * tip_percentage / 100
+total_bill = bill + tip_amount
+
+print("Bill Amount : ₹", bill)
+print("Tip Amount  : ₹", round(tip_amount, 2))
+print("Total Bill  : ₹", round(total_bill, 2))
