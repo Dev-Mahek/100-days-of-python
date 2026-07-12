efd6c450cf0f58739eb51431996853e1c5e9e9aa
Mahek Roy <dev-mahek@zohomail.in>
2026-07-12 07:43:38 +0530

    Update challenge.py

--- Day-002/challenge.py
+++ Day-002/challenge.py
@@ -1,1 +1,14 @@
+"""
+EMI Calculator
+"""
 
+loan = float(input("Enter loan amount: "))
+months = int(input("Enter number of months: "))
+
+emi = loan / months
+
+print("Loan Amount:", loan)
+print("Months:", months)
+print("Monthly EMI: ₹", round(emi, 2))
+
+"""
