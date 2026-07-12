ebe40918adabfc4af075b58455cc923792678417
Mahek Roy <dev-mahek@zohomail.in>
2026-07-12 07:50:50 +0530

    Update challenge.py

--- Day-002/challenge.py
+++ Day-002/challenge.py
@@ -24,3 +24,27 @@ total_bill = bill + tip_amount
 print("Bill Amount : ₹", bill)
 print("Tip Amount  : ₹", round(tip_amount, 2))
 print("Total Bill  : ₹", round(total_bill, 2))
+
+"""
+Shopping Bill Calculator
+"""
+
+item1 = float(input("Enter price of Item 1: ₹"))
+item2 = float(input("Enter price of Item 2: ₹"))
+item3 = float(input("Enter price of Item 3: ₹"))
+
+subtotal = item1 + item2 + item3
+
+gst = subtotal * 0.18
+
+if subtotal > 5000:
+    discount = subtotal * 0.10
+else:
+    discount = 0
+
+final_bill = subtotal + gst - discount
+
+print("Subtotal : ₹", subtotal)
+print("GST (18%): ₹", gst)
+print("Discount : ₹", discount)
+print("Final Bill: ₹", round(final_bill, 2))
