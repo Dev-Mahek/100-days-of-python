c1e53e54a4d05927f49f664bebb2403643e967fb
Mahek Roy <dev-mahek@zohomail.in>
2026-07-12 08:01:10 +0530

    Update practice.py

--- Day-003/practice.py
+++ Day-003/practice.py
@@ -1,3 +1,5 @@
+# Arithmetic Operators
+
 a = 10
 b = 3
 
@@ -8,3 +10,78 @@ print(a / b)
 print(a // b)
 print(a % b)
 print(a ** b)
+
+# Assignment Operators
+
+score = 50
+
+score += 10
+print(score)
+
+money = 100
+
+money += 50
+money -= 20
+money *= 2
+money //= 5
+
+print(money)
+
+# Comparison Operators
+
+"""
+True
+or
+False
+"""
+
+age = 20
+
+print(age == 20)
+print(age != 20)
+print(age > 18)
+print(age < 18)
+
+# Logical Operators
+
+age = 22
+student = True
+
+print(age > 18 and student)
+
+print(age < 18 or student)
+
+print(not student)
+
+age = 22
+student = True
+
+print(age > 18 and student)
+
+print(age < 18 or student)
+
+print(not student)
+
+# Identity Operators (is, is not)
+
+a = [1, 2]
+
+b = a
+
+c = [1, 2]
+
+print(a is b)
+
+print(a is c)
+
+print(a == c)
+
+# Membership Operators (in, not in)
+
+fruits = ["Apple", "Mango", "Banana"]
+
+print("Apple" in fruits)
+
+print("Orange" in fruits)
+
+print("Orange" not in fruits)
