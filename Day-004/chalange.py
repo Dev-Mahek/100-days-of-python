94a4014fb1c86868c2bdcb02d444546d2df4a1f7
Mahek Roy <dev-mahek@zohomail.in>
2026-07-14 20:31:10 +0530

    Create chalange.py

---
+++ Day-004/chalange.py
@@ -0,0 +1,23 @@
+"""
+Build a Restaurant Bill Discount Calculator.
+
+Requirements:
+
+Ask the user to enter the bill amount.
+If the bill is ₹5000 or more, give a 20% discount.
+Else if the bill is ₹2000 or more, give a 10% discount.
+Otherwise, give no discount.
+Print:
+Original bill
+Discount amount
+Final amount to pay
+
+Example:
+
+Enter bill amount: 3500
+
+Original Bill: ₹3500
+Discount: ₹350
+Final Bill: ₹3150
+"""
+
