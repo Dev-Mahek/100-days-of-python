e1c421c646d3cc40bd62138bc0e892125f5eaa21
Mahek Roy <dev-mahek@zohomail.in>
2026-07-22 13:42:31 +0530

    Update chalange.py

--- Day-006/chalange.py
+++ Day-006/chalange.py
@@ -1,1 +1,29 @@
+# Print each color on a new line.
 
+colors = ["Red", "Blue", "Green", "Yellow"]
+
+for color in colors:
+    print(color)
+
+"""
+Print:
+I love Python
+5 times using a loop.
+"""
+
+for i in range(5):
+    print("I love Python")
+
+"""
+Find the sum.
+marks = [78, 92, 85, 66, 90]
+"""
+
+marks = [78, 92, 85, 66, 90]
+
+total = 0
+
+for mark in marks:
+    total = total + mark
+
+print(total)
