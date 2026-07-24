dd737f57e7f2f6ee1663ef0fbc61799c2686744e
Mahek Roy <dev-mahek@zohomail.in>
2026-07-24 11:47:19 +0530

    Update challange.py

--- Day-007/challange.py
+++ Day-007/challange.py
@@ -1,1 +1,35 @@
+# Check if an Item Exists
 
+numbers = [5, 10, 15, 20, 25]
+
+if 15 in numbers:
+    print("Found")
+else:
+    print("Not Found")
+
+# Print Every Number
+
+numbers = [3, 6, 9, 12, 15]
+
+for num in numbers:
+    print(num)
+
+# Print Even Numbers
+
+numbers = [3, 6, 9, 12, 15]
+
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+
+# Find the Largest Number Without max()
+
+numbers = [3, 6, 9, 12, 15]
+
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("Largest:", largest)
