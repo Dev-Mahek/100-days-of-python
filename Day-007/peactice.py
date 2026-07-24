63af93e93cb5443d32f07a2c31747be46caa5cba
Mahek Roy <dev-mahek@zohomail.in>
2026-07-24 11:44:32 +0530

    Update peactice.py

--- Day-007/peactice.py
+++ Day-007/peactice.py
@@ -1,1 +1,35 @@
 fruits = ["Apple", "Banana", "Mango", "Orange"]
+print(fruits)
+
+print(fruits[0])
+print(fruits[-1])
+
+fruits[1] = "Grapes"
+
+print(fruits)
+
+
+fruits.append("Orange")
+fruits.append("Kiwi")
+
+
+fruits.insert(1, "Pineapple")
+
+print(fruits)
+
+numbers = [10, 20, 30, 40, 50]
+
+numbers.remove(30)
+
+print(numbers)
+
+
+numbers.pop()
+
+print(numbers)
+
+print("Length:", len(numbers))
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
+print("Sum:", sum(numbers))
+
